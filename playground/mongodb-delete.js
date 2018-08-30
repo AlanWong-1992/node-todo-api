03acf1589a1daac2ccc0a6be2ed730635e77936b
Alan Wong <alan_wong888@yahoo.com>
2018-08-30 16:44:31 +0100

    Added mongodb-delte.js file

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,28 @@
+// mongodb-connect.js
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB Server');
+	}
+
+	console.log('Connected to MongoDB Server');
+	const db = client.db('ToDoApp');
+
+	db.collection('Users').deleteMany({name: 'Alan'}).then((result) => {
+		console.log('Successfully deleted items');
+		console.log(JSON.stringify(result, undefined, 2));
+	}, (err) => {
+		console.log('Unable to delete items: ', err);
+	});
+
+	db.collection('Users').findOneAndDelete({_id: ObjectId("5b881072a68cbb26fa2452e8")}).then((result) => {
+		console.log('Successfully found one and deleted it');
+		console.log(result);
+	}, (err) => {
+		console.log('Unable to find and delete', err);
+	})
+
+	client.close();
+})
