286db19f867e19deb2484592a118e8b54777b3c9
Alan Wong <alan_wong888@yahoo.com>
2018-09-02 14:10:54 +0100

    Test GET /todos route

--- server/server.js
+++ server/server.js
@@ -18,33 +18,20 @@ app.post('/todos', (req, res) => {
 	todo.save().then((doc) => {
 		res.send(doc);
 	}, (err) => {
-		res.status(400).send(e);
+		res.status(400).send(err);
 	});
 });
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (err) => {
+		res.status(400).send(err);
+	})
+})
 
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
-// var newTodo = new Todo({
-// 	text: '          Go through Node Tutorials            ',
-// 	completed: true,
-// 	completedAt: 1200
-// });
 
-// newTodo.save().then((doc) => {
-// 	console.log('Saved todo', doc);
-// }, (err) => {
-// 	console.log('Unable to save todo:', err);
-// });
-
-// var newUser = new User({
-// 	name: 'Alan',
-// 	email: 'alan@email.com'
-// })
-
-// newUser.save().then((doc) => {
-// 	console.log('Saving new User into the db: ');
-// 	console.log(JSON.stringify(doc, undefined, 2));
-// }, (err) => {
-// 	console.log('Unable to save new user. ', err);
-// })
+module.exports = {app};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,10 +5,16 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {user} = require('./../models/user');
 
+const todos =[{
+	text: 'First Test to Do'
+},{
+	text: 'Second Test to Do'
+}];
+
 beforeEach((done) => {
 	Todo.remove({}).then(() => {
-		done();
-	});
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,7 +32,7 @@ describe('POST /todos', () => {
 					return done(err);
 				}
 
-				Todo.find().then((todos) => {
+				Todo.find({text}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -43,12 +49,26 @@ describe('POST /todos', () => {
 				if(err){
 					return done(err);
 				}
-				
+
 				Todo.find().then((todos) => {
-					expect(todos.length).toBe(0);
+					expect(todos.length).toBe(2);
 					done();
 				}).catch((err) => done(err));
 
 			});
 	});
 });
+
+describe('GET /todos', () => {
+	it('Should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+				expect(res.body.todos[0].text).toBe(todos[0].text);
+				expect(res.body.todos[1].text).toBe(todos[1].text);
+			})
+			.end(done);
+	})
+})
