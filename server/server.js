6d3844061ab4b238ac81eb3947d71b878fa250ca
Alan Wong <alan_wong888@yahoo.com>
2018-09-06 19:21:23 +0100

    Added POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -110,14 +110,14 @@ app.patch('/todos/:id', (req, res) => {
 
 // POST
 app.post('/users', (req, res) => {
-	// console.log(req.body);
-	// console.log('Token Access:', req.body.tokens[0].access);
-	// console.log('Token Token:', req.body.tokens[0].token);
+
 	var body = _.pick(req.body, ['email', 'password']);
 	var user = new User(body);
 
-	user.save().then((doc) => {
-		res.send(doc);
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
 	}).catch((err) => res.status(400).send(err));
 })
 
