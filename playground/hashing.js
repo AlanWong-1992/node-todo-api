831c0b4de5f16edcd125599c4e5bd0c6695460a9
Alan Wong <alan_wong888@yahoo.com>
2018-09-08 14:27:00 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(15, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
+var hashedPassword = '$2a$10$yBl0WRcdcm8dEaaemQ.Rmurec6.R5mahfqInBDHiv2CrRKc/538Oy';
+var hashedPassword2 = '$2a$15$0UZtxj1I4jHU29YIvUjnf.QLruhH/jCeXbeP7eztc5P0iEDh0Lj0C';
+
+bcrypt.compare(password, hashedPassword2, (err, res) => {
+	console.log(res); 
+})
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded:', decoded);
 
 
 
